96ea8c6239bf3937fa25778bf7f4479df94ae38c
AlessandroTatoli <vitoalessandro98@hotmail.com>
2018-10-24 22:24:58 -0400

    Arreglos

--- src/app/services/firestore/firestore.service.ts
+++ src/app/services/firestore/firestore.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+
+
 @Injectable({
   providedIn: 'root'
 })
